22a7ca719cc83053bfb2c850dbccd2bfa1ec5c51
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-27 14:45:36 +0000

    create log.txt with code

--- Semana4.py
+++ Semana4.py
@@ -1,7 +1,20 @@
 file = open("lorem.txt", "rt")
 
-first_line = file.readline()
+first_line = file.readlines()
 
-print(first_line)
+for line in first_line:
+    print(line.strip())
+
 
 file.close()
+
+#Modos de abertura
+#r, w, a, t, b
+
+file2 = open("log.txt", "wt")
+
+for i in range(0, 10):
+    txt = ("Line" + str(i))
+    file2.write(txt)
+    
+file2.close()
