f3060d31c6183e759c0a1ccb188f11b99dc23689
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-29 13:14:24 +0000

    add encode

--- read_json.py
+++ read_json.py
@@ -3,14 +3,15 @@ import sys
 
 def read_json(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding="utf-8") as file:
             data = json.load(file)
             print(data)
+    except UnicodeDecodeError:
+        print("Erro de decodificação! O arquivo pode não estar em UTF-8.")
     except Exception:
         print("Ocorreu um erro!")
     finally:
         print("Processo concluído!")
-
 
 if len(sys.argv) != 2:
     print("Uso: python read_json.py <caminho_para_o_arquivo_json>")
