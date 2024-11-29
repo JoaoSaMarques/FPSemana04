7694dee78f98e323dcb5258a48319edb08da2255
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-29 13:05:18 +0000

    add in case of error

--- read json.py
+++ read json.py
@@ -2,6 +2,17 @@ import json
 import sys
 
 def read_json(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-        print(data)
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            print(data)
+    except Exception:
+        print("Ocorreu um erro!")
+    finally:
+        print("Processo concluído!")
+
+
+if len(sys.argv) != 2:
+    print("Uso: python read_json.py <caminho_para_o_arquivo_json>")
+else:
+    read_json(sys.argv[1])
